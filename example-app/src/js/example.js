9aaef74387534e7f78607b53167adb0af70aef9f
Jean-Baptiste Malatrasi <jb.malatrasi@gmail.com>
2025-03-05 17:52:46 +0100

    wip

--- example-app/src/js/example.js
+++
@@ -1,6 +0,0 @@
-import { WakoCapacitorVideoPlayer } from 'wako-capacitor-video-player';
-
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    WakoCapacitorVideoPlayer.echo({ value: inputValue })
-}
